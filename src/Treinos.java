ec7881935ebe3a1f1318d113bec59ac82f990380
danillojosec <danillo_nice@hotmail.com>
2023-08-23 16:54:23 -0300

    Implementação do método HeapSort

--- src/Treinos.java
+++ src/Treinos.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class Treinos {
     public static void main(String[] args) {
+        
         int target = 30;
         List<Integer> lista = new ArrayList<>();
         lista.add(9);lista.add(22);lista.add(30);lista.add(5);
@@ -11,9 +12,40 @@ public class Treinos {
 
         System.out.println("Lista: \t\t" + lista);
         //Collections.sort(lista);
-        quickSort(lista);
+        //quickSort(lista);
+        heapSort(lista);
         System.out.println("Lista ordenada: " + lista);
         System.out.println(buscaBinaria(lista, target));
+    }
+
+    public static void heapSort(List lista){
+        /* Presume-se que a árvore é binária. */
+        
+        if(lista.size() > 1){
+            for(int pai = 0; pai < lista.size(); pai++){
+                int filho1 = pai*2 + 1; //filho da esquerda
+                int filho2 = pai*2 + 2; //filho da direita
+                int maior_filho = 0;
+
+                if(filho2 < lista.size() && (int) lista.get(filho2) > (int) lista.get(filho1)){
+                    maior_filho = filho2;
+                }
+                else {
+                    if(filho1 < lista.size()){
+                        maior_filho = filho1;
+                    }
+                }
+                if(maior_filho != 0 && (int) lista.get(pai) < (int) lista.get(maior_filho)){
+                    Collections.swap(lista, pai, maior_filho);
+                }
+            }
+
+            //System.out.println("Heap máxima: \t" + lista);
+            Collections.swap(lista, 0, lista.size()-1);
+            heapSort(lista.subList(0, lista.size()-1));
+
+        }
+
     }
 
     public static void quickSort(List lista){
